be5d048094fb7445275979c039a4f7e081cd0a58
Burak İpekci <76015489+Burakiipekci@users.noreply.github.com>
2021-02-13 23:57:03 +0300

    Add project files.

---
+++ DataAccsess/Concrete/EntityFramwork/EfOrderDal.cs
@@ -0,0 +1,12 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramwork
+{
+    public class EfOrderDal : EfEntityRepositoryBase<Order,NorthwindContext>,IOrderDal
+    {
+    }
+}
